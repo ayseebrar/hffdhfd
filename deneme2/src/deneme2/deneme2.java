3d6de23bafafa33842d2a24b108e06828fa3bdf4
lenovo <lenovo@10.48.77.106>
2022-10-27 11:39:53 +0300

    fisrt

---
+++ deneme2/src/deneme2/deneme2.java
@@ -0,0 +1,18 @@
+package deneme2;
+
+public class deneme2 {
+
+	public static void main(String[] args) {
+		final int ARRAY_LENGHT =10;
+		int[] array = new int[ARRAY_LENGHT];
+		for(int counter = 0; counter<array.length; counter++) {
+			array[counter]= 2+2*counter;
+		}
+ System.out.printf("%s%8s%n", "Index","Value");
+ for(int counter=0; counter<array.length; counter++) {
+	 System.out.printf("%5d%8d%n", counter, array[counter]);
+	 System.out.println("");
+ }
+	}
+
+}
---
+++ deneme2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lenovo
+ *
+ */
+module deneme2 {
+}
